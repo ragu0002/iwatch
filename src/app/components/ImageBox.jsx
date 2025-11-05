fb7baa19fe2abd2a2a879e879d1c91197b12698a
Ragnhild <ragu0002@stud.kea.dk>
2025-11-05 13:38:07 +0100

    feat-ragnhild-product-box-to-tailwind

--- src/app/components/ImageBox.jsx
+++ src/app/components/ImageBox.jsx
@@ -3,17 +3,41 @@ import Image from "./Image";
 const ImageBox = ({ setSelectedImage }) => {
   return (
     <div className="col-1 flex items-center justify-center gap-20">
-      <button onClick={() => setSelectedImage("navy")}>
+      <button
+        onClick={() => setSelectedImage("navy")}
+        className="after:bg-navy/50 relative after:absolute after:bottom-0 after:left-0 after:z-10 after:h-[70px] after:w-[120px] after:rounded-2xl after:content-['']"
+      >
         {" "}
-        <Image width={100} height={100} img={"/navy.png"} />
+        <Image
+          width={100}
+          height={100}
+          img={"/navy.png"}
+          classname={`z-20 relative`}
+        />
       </button>
-      <button onClick={() => setSelectedImage("ocean")}>
+      <button
+        onClick={() => setSelectedImage("ocean")}
+        className="after:bg-ocean/50 relative after:absolute after:bottom-0 after:left-0 after:z-10 after:h-[70px] after:w-[120px] after:rounded-2xl after:content-['']"
+      >
         {" "}
-        <Image width={100} height={100} img={"/ocean.png"} />
+        <Image
+          width={100}
+          height={100}
+          img={"/ocean.png"}
+          classname={`z-20 relative`}
+        />
       </button>
-      <button onClick={() => setSelectedImage("mint")}>
+      <button
+        onClick={() => setSelectedImage("mint")}
+        className="after:bg-mint/50 relative after:absolute after:bottom-0 after:left-0 after:z-10 after:h-[70px] after:w-[120px] after:rounded-2xl after:content-['']"
+      >
         {" "}
-        <Image width={100} height={100} img={"/mint.png"} />
+        <Image
+          classname={`z-20 relative`}
+          width={100}
+          height={100}
+          img={"/mint.png"}
+        />
       </button>
     </div>
   );
--- src/app/components/ProductBox.jsx
+++ src/app/components/ProductBox.jsx
@@ -9,18 +9,29 @@ const ProductBox = () => {
     <div className="grid grid-cols-[1fr_auto] justify-center">
       <Image width={500} height={500} img={`/${selectedImage}.png`} />
       <ImageBox setSelectedImage={setSelectedImage} />
-      <div className="col-2 row-1 grid justify-evenly gap-20 self-center">
+      <div className="col-2 row-1 grid content-center justify-items-center gap-0.5">
         <div
           onClick={() => setSelectedImage("navy")}
-          className={`h-4 w-4 rounded-full ${selectedImage === `navy` ? `bg-navy outline-3 outline-white outline-solid` : `bg-navy`}`}
+          className={`m-0.5 h-4 w-4 rounded-full ${selectedImage === `navy` ? `bg-navy h-5 w-5 shadow-xl/30 outline-3 outline-white outline-solid` : `bg-navy outline-1 outline-white outline-solid`}`}
         ></div>
+        <div className="h-0.5 w-0.5 rounded-full bg-white"></div>
+        <div className="h-0.5 w-0.5 rounded-full bg-white"></div>
+        <div className="h-0.5 w-0.5 rounded-full bg-white"></div>
+        <div className="h-0.5 w-0.5 rounded-full bg-white"></div>
+        <div className="h-0.5 w-0.5 rounded-full bg-white"></div>
+
         <div
           onClick={() => setSelectedImage("ocean")}
-          className={`h-4 w-4 rounded-full ${selectedImage === `ocean` ? `bg-ocean outline-3 outline-white outline-solid` : `bg-ocean`}`}
+          className={`m-0.5 h-4 w-4 rounded-full ${selectedImage === `ocean` ? `bg-ocean h-5 w-5 shadow-xl/30 outline-3 outline-white outline-solid` : `bg-ocean outline-1 outline-white outline-solid`}`}
         ></div>
+        <div className="h-0.5 w-0.5 rounded-full bg-white"></div>
+        <div className="h-0.5 w-0.5 rounded-full bg-white"></div>
+        <div className="h-0.5 w-0.5 rounded-full bg-white"></div>
+        <div className="h-0.5 w-0.5 rounded-full bg-white"></div>
+        <div className="h-0.5 w-0.5 rounded-full bg-white"></div>
         <div
           onClick={() => setSelectedImage("mint")}
-          className={`h-4 w-4 rounded-full ${selectedImage === `mint` ? `bg-mint outline-3 outline-white outline-solid` : `bg-mint`}`}
+          className={`m-0.5 h-4 w-4 rounded-full ${selectedImage === `mint` ? `bg-mint h-5 w-5 shadow-xl/30 outline-3 outline-white outline-solid` : `bg-mint outline-1 outline-white outline-solid`}`}
         ></div>
       </div>
     </div>
